c8fa08c4fd439c227f4676408f5957343ec3e600
md tausif <tausifsmo@gmail.com>
2023-01-23 23:16:17 +0530

    api url change

--- scr/Products/Controller/SortingController.js
+++ scr/Products/Controller/SortingController.js
@@ -2,15 +2,18 @@ const Model = require("../Models/ProductModel");
 
 const filterPrice = async (req, res) => {
   try {
-    const price = req.query.sort;
+    const category = req.query.category;
+    const price = req.query.price;
     const size = req.query.size || 10;
     const page = req.query.page || 1;
+    // console.log(req.query)
     let products = await Model.find({
-      selling_price: { $gt: price - 1000, $lt: price },
+      category,
+      selling_price: { $gt: parseInt(price) - 1000, $lt: parseInt(price) },
     })
       .skip((page - 1) * size)
-      .limit(size)
-    console.log("product", page,size,price)
+      .limit(size);
+    // console.log("product",products);
     return res.status(200).json({
       success: true,
       products,
@@ -22,4 +25,63 @@ const filterPrice = async (req, res) => {
     });
   }
 };
-module.exports = { filterPrice };
+const filterDiscount = async (req, res) => {
+  try {
+    const category = req.query.category;
+    const discount = req.query.discount;
+    const size = req.query.size || 10;
+    const page = req.query.page || 1;
+    // console.log("product", req.query);
+    let products = await Model.find({
+      category,
+      discount,
+      //   discount: { $gt: discount - 10, $lt: discount },
+      //   discount: { $gt: parseInt(discount) - 10, $lt: parseInt(discount) },
+    })
+      .skip((page - 1) * size)
+      .limit(size);
+
+    // console.log(products);
+    return res.status(200).json({
+      success: true,
+      products,
+    });
+  } catch (err) {
+    return res.status(200).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+const filtersubCategory = async (req, res) => {
+  try {
+    const category = req.query.category;
+    const discount = req.query.discount;
+    const size = req.query.size || 10;
+    const page = req.query.page || 1;
+    // console.log("product", req.query);
+    const obj = {
+      sleeve: discount,
+      pattern: discount,
+      occasion: discount,
+      neck: discount,
+      category,
+    };
+    console.log(obj);
+    let products = await Model.find(obj)
+      .skip((page - 1) * size)
+      .limit(size);
+    console.log(products);
+    return res.status(200).json({
+      success: true,
+      products,
+    });
+  } catch (err) {
+    return res.status(200).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+module.exports = { filterPrice, filterDiscount, filtersubCategory };
--- scr/Products/Routes/AllProductRoutes.js
+++ scr/Products/Routes/AllProductRoutes.js
@@ -4,12 +4,18 @@ const {
   getAllProducts,
   getProductId,
 } = require("../Controller/getproductdata");
-const { filterPrice } = require("../Controller/SortingController");
+const {
+  filterPrice,
+  filterDiscount,
+  filtersubCategory,
+} = require("../Controller/SortingController");
 
 const routes = require("express").Router();
 routes.get("/productsall", getAllProducts);
 routes.get("/products", getProducts);
 routes.get("/product/:id", getProductId);
 routes.get("/products/filterPrice", filterPrice);
+routes.get("/products/filterdiscount", filterDiscount);
+routes.get("/products/filtersubcategory", filtersubCategory);
 
 module.exports = routes;
